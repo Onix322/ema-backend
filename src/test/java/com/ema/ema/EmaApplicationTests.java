3453d81ed88074512c75e81d7bc9cda3aa9765df
alex <alexandru.dobos3@gmail.com>
2025-10-25 22:22:55 +0200

    created spring boot application

---
+++ src/test/java/com/ema/ema/EmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ema.ema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
